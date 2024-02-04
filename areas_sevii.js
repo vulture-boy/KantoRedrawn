617563bad61e48ef220aebd30d560a1b2c222de8
Blindeduif <jordi.boin@gmail.com>
2024-02-04 14:38:53 +0100

    start of sevii js

--- areas_sevii.js
+++ areas_sevii.js
@@ -1,1 +1,53 @@
-var seviiAreas = [] 
+var seviiAreas = [
+  {
+    order: 0,
+    title: "Five Island Memorial Pillar",
+    ident: "Five-Island-Memorial-Pillar",
+    artist: "",
+    artistImageOverride: "",
+    url: "",
+    animation: false,
+    point: {
+      x: 0,
+      y: 0
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: ""
+  },
+  
+    {
+    order: 1,
+    title: "Resort Gorgeous",
+    ident: "Five-Island-Resort-Gorgeous",
+    artist: "@Blindeduif",
+    artistImageOverride: "",
+    url: "https://twitter.com/blindeduif",
+    animation: false,
+    point: {
+      x: 0,
+      y: 0
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: "https://twitter.com/blindeduif/status/1389625876720070656"
+  },
+
+
+] 
