bcc4bc6cd51758b02eca8552de01e25e12b6d664
Blindeduif <jordi.boin@gmail.com>
2024-02-05 15:12:34 +0100

    First half sevii JS

--- areas_sevii.js
+++ areas_sevii.js
@@ -1,11 +1,11 @@
 var seviiAreas = [
   {
     order: 0,
-    title: "Five Island Memorial Pillar",
-    ident: "Five-Island-Memorial-Pillar",
-    artist: "",
+    title: "One Island Mt. Ember",
+    ident: "One-Island-Mt-Ember",
+    artist: "@Dewndeym",
     artistImageOverride: "",
-    url: "",
+    url: "https://twitter.com/Dewndeym",
     animation: false,
     point: {
       x: 0,
@@ -21,11 +21,283 @@ var seviiAreas = [
     type: "route",
     zoom: 1,
     teleporters: [],
+    post_url: "https://twitter.com/Dewndeym/status/1393800531047485441"
+  },
+  {
+    order: 1,
+    title: "One Island Town",
+    ident: "One-Island-Town",
+    artist: "@Anima_nel",
+    artistImageOverride: "",
+    url: "https://twitter.com/Anima_nel",
+    animation: false,
+    point: {
+      x: 0,
+      y: 2432
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: "https://twitter.com/Anima_nel/status/1547326349253148674"
+  },
+  
+  {
+    order: 2,
+    title: "One Island Treasure Beach",
+    ident: "One-Island-Treasure-Beach",
+    artist: "@KarlestonChew"
+    artistImageOverride: "",
+    url: "https://twitter.com/KarlestonChew",
+    animation: false,
+    point: {
+      x: 0,
+      y: 2720
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: "https://twitter.com/KarlestonChew/status/1482883281808986113"
+  },
+  {
+   order: 3,
+    title: "One Island Kindle Road",
+    ident: "One-Island-Kindle-Road",
+    artist: "@Dewndeym",
+    artistImageOverride: "",
+    url: "https://twitter.com/Dewndeym",
+    animation: false,
+    point: {
+      x: 0,
+      y: 1136
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
     post_url: ""
   },
   
+   {
+   order: 4,
+    title: "Two Island Town",
+    ident: "Two-Island-Town",
+    artist: "@Unsettled",
+    artistImageOverride: "",
+    url: "https://twitter.com/_unsettled_",
+    animation: false,
+    point: {
+      x: 592,
+      y: 2464
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: "https://twitter.com/_unsettled_/status/1403027122223001611"
+  },
+  
+   {
+   order: 5,
+    title: "Two Island: Cape Brink",
+    ident: "Two-Island-Cape-Brink",
+    artist: "@Paprika_d",
+    artistImageOverride: "",
+    url: "https://twitter.com/Paprika_d",
+    animation: false,
+    point: {
+      x: 912,
+      y: 2080
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: "https://twitter.com/Paprika_d/status/1481656412463722506"
+  },
+
+   {
+   order: 6,
+    title: "Three Island Town",
+    ident: "Three-Island-Town",
+    artist: "@HaywireJo",
+    artistImageOverride: "",
+    url: "https://twitter.com/HaywireJo",
+    animation: false,
+    point: {
+      x: 2896,
+      y: 1824
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: "https://twitter.com/HaywireJo/status/1370096913681416194"
+  },
+
+   {
+   order: 7,
+    title: "Three Island: Bond Bridge",
+    ident: "Three-Island-Bond-Bridge",
+    artist: "@Zikoure",
+    artistImageOverride: "",
+    url: "https://twitter.com/SuperZikoure",
+    animation: false,
+    point: {
+      x: 1584,
+      y: 1824
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: ""
+  },
+   {
+   order: 8,
+    title: "Four Island Town",
+    ident: "Four-Island-Town",
+    artist: "@StrawBe",
+    artistImageOverride: "",
+    url: "https://twitter.com/StrawPact",
+    animation: false,
+    point: {
+      x: 0,
+      y: 3472
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: "https://twitter.com/StrawPact/status/1459286160727969796"
+  },
+  
+   {
+   order: 9,
+    title: "Five Island Town and Meadow",
+    ident: "Five-Island-Town-and-Meadow",
+    artist: "@StrawBe",
+    artistImageOverride: "",
+    url: "https://twitter.com/StrawPact",
+    animation: false,
+    point: {
+      x: 1760,
+      y: 3936
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: ""
+  },
+
+  {
+    order: 10,
+    title: "Five Island Memorial Pillar",
+    ident: "Five-Island-Memorial-Pillar",
+    artist: "@StrawBe",
+    artistImageOverride: "",
+    url: "https://twitter.com/StrawPact",
+    animation: false,
+    point: {
+      x: 2400,
+      y: 4208
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: "https://twitter.com/StrawPact/status/1436843389195104257"
+  },
+  
+   {
+    order: 11,
+    title: "Five Island: Water Labyrinth",
+    ident: "Five-Island-Water-Labyrinth",
+    artist: "@toaster",
+    artistImageOverride: "",
+    url: "https://twitter.com/1000_toasters",
+    animation: false,
+    point: {
+      x: 1104,
+      y: 3664
+    },
+    offset: {
+      x: null,
+      y: null,
+      width: null,
+      height: null
+    },
+    pan: "vertical",
+    type: "route",
+    zoom: 1,
+    teleporters: [],
+    post_url: "https://twitter.com/1000_toasters/status/1599805934980673539"
+  },
+  
     {
-    order: 1,
+    order: 12,
     title: "Resort Gorgeous",
     ident: "Five-Island-Resort-Gorgeous",
     artist: "@Blindeduif",
@@ -33,8 +305,8 @@ var seviiAreas = [
     url: "https://twitter.com/blindeduif",
     animation: false,
     point: {
-      x: 0,
-      y: 0
+      x: 1760,
+      y: 3392
     },
     offset: {
       x: null,
